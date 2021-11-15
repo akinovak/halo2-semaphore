749aededf07775c74c8d2117ab2a599eb31c8037
Andrija Novakovic <andrijanovakovic@Andrijas-MacBook-Pro.local>
2021-11-15 13:46:08 +0100

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -153,31 +153,6 @@ impl Circuit<pallas::Base> for SemaphoreCircuit
         let add_chip = config.construct_add_chip();
         let merkle_chip = config.construct_merkle_chip();
 
-        let poseidon_chip = config.construct_poseidon_chip();
-        let poseidon_chip_2 = config.construct_poseidon_chip();
-
-        let mut poseidon_hasher: PoseidonHash
-        <
-            Fp, 
-            PoseidonChip<Fp>, 
-            P128Pow5T3, 
-            ConstantLength<2_usize>, 
-            3_usize, 
-            2_usize
-        > 
-            = PoseidonHash::init(poseidon_chip, layouter.namespace(|| "init hasher"), ConstantLength::<2>)?;
-
-        let mut poseidon_hasher_2: PoseidonHash
-            <
-                Fp, 
-                PoseidonChip<Fp>, 
-                P128Pow5T3, 
-                ConstantLength<2_usize>, 
-                3_usize, 
-                2_usize
-            > 
-                = PoseidonHash::init(poseidon_chip_2, layouter.namespace(|| "init hasher"), ConstantLength::<2>)?;
-
         let identity_trapdoor = self.load_private(
             layouter.namespace(|| "witness identity_trapdoor"),
             config.advices[0],
@@ -210,7 +185,7 @@ impl Circuit<pallas::Base> for SemaphoreCircuit
             "identity commitment"
         )?;
 
-        println!("Identity Commitment: {:?}", identity_commitment.value());
+        // println!("Identity Commitment: {:?}", identity_commitment.value());
 
         let nullifier_hash_message = [identity_nullifier, external_nulifier];
         let nullifier_hash = self.hash(
@@ -220,38 +195,7 @@ impl Circuit<pallas::Base> for SemaphoreCircuit
             "nullifier hash"
         )?;
 
-        println!("Nullifier hash: {:?}", nullifier_hash.value());
-
-
-        // let identity_commitment_loaded = poseidon_hasher.witness_message_pieces(
-        //     config.poseidon_config.clone(),
-        //     layouter.namespace(|| "identity commitment hash"),
-        //     identity_commitment_message
-        // )?;
-
-        // let identity_commitment_word = poseidon_hasher.hash(layouter.namespace(|| "hash to identity commitment"), identity_commitment_loaded, "identity commitment")?;
-        // let identity_commitment: CellValue<Fp> = identity_commitment_word.inner().into();
-
-        // println!("{:?}", identity_commitment.value());
-
-
-        // let nullifier_message = [identity_nullifier, external_nulifier];
-
-        // let nullifier_loaded = poseidon_hasher_2.witness_message_pieces(
-        //     config.poseidon_config.clone(),
-        //     layouter.namespace(|| "nullifier hash"),
-        //     nullifier_message
-        // )?;
-
-        // let nullifier_hash_word = poseidon_hasher_2.hash(layouter.namespace(|| "hash to nullifier hash"), nullifier_loaded, "nullifier hash")?;
-        // let nullifier_hash: CellValue<Fp> = nullifier_hash_word.inner().into();
-
-        // println!("From circuit: {:?}", nullifier_hash.value());
-    
-
-        // let identity_commitment = add_chip.add(layouter.namespace(|| "commitment"), identity_nullifier, identity_trapdoor)?;
-        // let nullifier_hash = add_chip.add(layouter.namespace(|| "nullifier"), identity_nullifier, external_nulifier)?;
-
+        // println!("Nullifier hash: {:?}", nullifier_hash.value());
 
         // let merkle_inputs = MerklePath {
         //     chip: merkle_chip,
@@ -265,8 +209,8 @@ impl Circuit<pallas::Base> for SemaphoreCircuit
         // )?;
 
         
-        // self.expose_public(layouter.namespace(|| "constrain external_nullifier"), config.instance, external_nulifier, EXTERNAL_NULLIFIER)?;
-        // self.expose_public(layouter.namespace(|| "constrain nullifier_hash"), config.instance, nullifier_hash, NULLIFIER_HASH)?;
+        self.expose_public(layouter.namespace(|| "constrain external_nullifier"), config.instance, external_nulifier, EXTERNAL_NULLIFIER)?;
+        self.expose_public(layouter.namespace(|| "constrain nullifier_hash"), config.instance, nullifier_hash, NULLIFIER_HASH)?;
         // self.expose_public(layouter.namespace(|| "constrain root"), config.instance, calculated_root, ROOT)?;
 
         Ok({})
@@ -289,13 +233,9 @@ fn main() {
     let path = [Fp::from(1), Fp::from(1), Fp::from(1), Fp::from(1)];
     let position_bits = [Fp::from(0), Fp::from(1), Fp::from(0), Fp::from(1)];
     let identity_commitment = identity_trapdoor + identity_nullifier;
-    let message = [identity_trapdoor, identity_nullifier];
-    // let identity_commitment = PoseidonHash::init(P128Pow5T3, ConstantLength::<2>).hash(message);
-    // let nullifier_hash = identity_nullifier + external_nullifier;
 
     let message = [identity_nullifier, external_nullifier];
     let nullifier_hash = Hash::init(P128Pow5T3, ConstantLength::<2>).hash(message);
-    println!("From main: {:?}", Some(nullifier_hash));
 
     let root = identity_commitment + Fp::from(4);
 
@@ -315,7 +255,7 @@ fn main() {
     assert_eq!(prover.verify(), Ok(()));
 
     // If we try some other public input, the proof will fail!
-    // public_inputs[0] += Fp::one();
-    // let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
-    // assert!(prover.verify().is_err());
+    public_inputs[0] += Fp::one();
+    let prover = MockProver::run(k, &circuit, vec![public_inputs]).unwrap();
+    assert!(prover.verify().is_err());
 }
